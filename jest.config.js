c3cbb3b631f56717c00ac8ff91e443a689eb8ae0
Denis Grankin <liquidautumn@gmail.com>
2022-08-31 00:05:55 +0200

    Fix types, update dev dependencies, replace ts-jest with @swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,36 @@
 module.exports = {
   rootDir: './src',
   moduleNameMapper: {
-    "nest-casl": "<rootDir>/index.ts"
+    'nest-casl': '<rootDir>/index.ts',
   },
-  preset: 'ts-jest',
   moduleFileExtensions: ['js', 'json', 'ts'],
   coveragePathIgnorePatterns: ['node_modules', 'src/__specs__'],
   testRegex: '.spec.ts$',
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.(t|j)s$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            decorators: true,
+            topLevelAwait: true,
+          },
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true,
+          },
+          target: 'es2018',
+          externalHelpers: false,
+          keepClassNames: true,
+        },
+        module: {
+          type: 'commonjs',
+        },
+        sourceMaps: 'inline',
+      },
+    ],
   },
   testEnvironment: 'node',
   resetMocks: true,
-  globals: {
-    'ts-jest': {
-      tsconfig: './src/__specs__/tsconfig.json'
-    },
-  },
 };
